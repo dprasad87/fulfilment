8847c275357665ec9904d6644c11eca02198804a
dwarika <dwarika.prasad87@gmail.com>
2019-04-22 07:53:25 +0530

    Moved fulfilment logic to service class

--- orchestration/src/main/java/com/dworld/learn/orchestration/controllers/FulfilmentController.java
+++ orchestration/src/main/java/com/dworld/learn/orchestration/controllers/FulfilmentController.java
@@ -1,13 +1,8 @@
 package com.dworld.learn.orchestration.controllers;
 
-import com.dworld.learn.orchestration.constants.Constants;
-import com.dworld.learn.orchestration.models.FulfilmentDates;
 import com.dworld.learn.orchestration.models.FulfilmentOptions;
 import com.dworld.learn.orchestration.models.Order;
-import com.dworld.learn.orchestration.services.CarrierService;
-import com.dworld.learn.orchestration.services.FulfimentSourceService;
-import com.dworld.learn.orchestration.services.ProductService;
-import com.dworld.learn.orchestration.services.StockService;
+import com.dworld.learn.orchestration.services.FulfimentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,34 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class FulfilmentController {
 
     @Autowired
-    FulfimentSourceService fulfilmentSourceService;
-    @Autowired
-    ProductService productService;
-    @Autowired
-    StockService stockService;
-    @Autowired
-    CarrierService carrierService;
+    FulfimentService fulfimentService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/fulfilment-options")
     public FulfilmentOptions getFulfilmentOptions(@RequestBody  Order order) {
-        FulfilmentOptions fulfilmentOptions = new FulfilmentOptions();
-        fulfilmentOptions.setOrderId(order.getOrderId());
-        fulfilmentOptions.setProductDetails(productService.getProductDetails(order.getProductId()));
-        String fulfilmentSource = fulfilmentSourceService.getFulfilmentSource(order.getProductId());
-        if(fulfilmentSourceService.validateFulfilmentSource()) {
-            fulfilmentOptions.setFulfilmentSourceStatus(Constants.FULFILMENT_SOURCE_MATCH);
-            if(stockService.validateStock()) {
-                FulfilmentDates fulfilmentDates = carrierService.getFulfilmentDates(order.getProductId(), order.getQty(), fulfilmentSource, order.getAddress().getPincode());
-                fulfilmentOptions.setFulfilmentFromDate(fulfilmentDates.getFromDate());
-                fulfilmentOptions.setFulfilmentToDate(fulfilmentDates.getToDate());
-                fulfilmentOptions.setFulfilmentSource(fulfilmentSource);
-                fulfilmentOptions.setStockStatus(Constants.STOCK_AVAILABLE);
-            } else {
-                fulfilmentOptions.setStockStatus(Constants.STOCK_OUT_OF_STOCK);
-            }
-        } else {
-            fulfilmentOptions.setFulfilmentSourceStatus(Constants.FULFILMENT_SOURCE_MISMATCH);
-        }
-        return fulfilmentOptions;
+
+        return  fulfimentService.getFulfilmentOptions(order);
     }
 }
---
+++ orchestration/src/main/java/com/dworld/learn/orchestration/services/FulfimentService.java
@@ -0,0 +1,52 @@
+package com.dworld.learn.orchestration.services;
+
+import com.dworld.learn.orchestration.constants.Constants;
+import com.dworld.learn.orchestration.models.FulfilmentDates;
+import com.dworld.learn.orchestration.models.FulfilmentOptions;
+import com.dworld.learn.orchestration.models.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FulfimentService {
+
+    @Autowired
+    ProductService productService;
+    @Autowired
+    StockService stockService;
+    @Autowired
+    CarrierService carrierService;
+
+    public String getFulfilmentSource(String productId) {
+        return "Aditya Textile";
+    }
+
+
+
+    public boolean validateFulfilmentSource() {
+        return false;
+    }
+
+    public FulfilmentOptions getFulfilmentOptions(Order order) {
+
+        FulfilmentOptions fulfilmentOptions = new FulfilmentOptions();
+        fulfilmentOptions.setOrderId(order.getOrderId());
+        fulfilmentOptions.setProductDetails(productService.getProductDetails(order.getProductId()));
+        String fulfilmentSource = getFulfilmentSource(order.getProductId());
+        if(validateFulfilmentSource()) {
+            fulfilmentOptions.setFulfilmentSourceStatus(Constants.FULFILMENT_SOURCE_MATCH);
+            if(stockService.validateStock()) {
+                FulfilmentDates fulfilmentDates = carrierService.getFulfilmentDates(order.getProductId(), order.getQty(), fulfilmentSource, order.getAddress().getPincode());
+                fulfilmentOptions.setFulfilmentFromDate(fulfilmentDates.getFromDate());
+                fulfilmentOptions.setFulfilmentToDate(fulfilmentDates.getToDate());
+                fulfilmentOptions.setFulfilmentSource(fulfilmentSource);
+                fulfilmentOptions.setStockStatus(Constants.STOCK_AVAILABLE);
+            } else {
+                fulfilmentOptions.setStockStatus(Constants.STOCK_OUT_OF_STOCK);
+            }
+        } else {
+            fulfilmentOptions.setFulfilmentSourceStatus(Constants.FULFILMENT_SOURCE_MISMATCH);
+        }
+        return fulfilmentOptions;
+    }
+}
